b95c6e67116683db57a0c88d09992fb23e741013
lucasleaopinto <lucasleaop@gmail.com>
2019-09-20 21:32:53 -0300

    ultimo

--- app/src/main/java/br/edu/ifsp/scl/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.edu.ifsp.scl.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import br.edu.ifsp.scl.R
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -19,14 +21,25 @@ class MainActivity : AppCompatActivity() {
         //Fragment
         supportFragmentManager.beginTransaction().replace(R.id.calculadoraFl, CalculadoraBasicaFragment()).commit()
 
-
-
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var retorno = false
+        when(item.itemId){
+            R.id.sairMenuItem ->{
+                retorno = true
+                finish()
+            }
+            R.id.configuracoesMenuItem ->{
+                retorno = true
+                val configuracaoIntent = Intent(this,ConfiguracaoActivity::class.java)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
